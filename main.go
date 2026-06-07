c06be76904c378dccb5667008e729c314590ba8a
AndrewTHowell <andrewthomashowell@gmail.com>
2026-06-07 17:12:04 +0100

    Refactor structure

--- main.go
+++ main.go
@@ -1,37 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-	"os"
-)
+import "fmt"
 
 func main() {
-	println("Hello, World!")
-}
-
-func Save(path string, data []byte) error {
-	tmpFilePath := fmt.Sprintf("%s.tmp.%04d", path, rand.Intn(10000))
-	tmpFile, err := os.OpenFile(tmpFilePath, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0644)
-	if err != nil {
-		return fmt.Errorf("opening tmp file: %w", err)
-	}
-	defer func() {
-		// Close the file. This shouldn't error as it only errors if already closed.
-		tmpFile.Close()
-
-		if err != nil {
-			os.Remove(tmpFilePath)
-		}
-	}()
-
-	if _, err = tmpFile.Write(data); err != nil {
-		return fmt.Errorf("writing tmp file: %w", err)
-	}
-	if err = tmpFile.Sync(); err != nil {
-		return fmt.Errorf("syncing tmp file: %w", err)
-	}
-	// Save error to err var to ensure defer sees it.
-	err = os.Rename(tmpFilePath, path)
-	return err
+	fmt.Println("Hello, World!")
 }
